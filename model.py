1f5164fc2d85dc0d3ccb72d723f4cb76acf26e81
Grishma-Modi <66726953+Grishma-Modi@users.noreply.github.com>
2020-06-27 19:51:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
-dataset = pd.read_csv('C:/Users/Sarjan/Desktop/python/ML/github/Heroku-Demo-master/hiring.csv')
+dataset = pd.read_csv('hiring.csv')
 
 dataset['experience'].fillna(0, inplace=True)
 dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
